0ca1a57781673c2c4ecc115717767544af738aa7
Rishabh Shukla <rishabhcodershukla@gmail.com>
2022-07-07 02:34:32 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express()
 const port = 9000
 
-const ZK_HOST = "http://localhost:3002/api/getId"
+const ZK_HOST = "https://zk-oauth.vercel.app/api/getId"
 
 app.get('/', (req, res) => {
 	res.sendFile('views/index.html', {root: __dirname })
