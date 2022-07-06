c99270560984cbc497e900345abebc5c5da2085b
Rishabh Shukla <rishabhcodershukla@gmail.com>
2022-07-07 02:23:43 +0530

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+const app = express()
+const port = 9000
+
+const ZK_HOST = "http://localhost:3002/api/getId"
+
+app.get('/', (req, res) => {
+	res.sendFile('views/index.html', {root: __dirname })
+})
+
+app.get('/verify', (req,res) => {
+	const token = req.query.token
+	//console.log(token)
+	axios.get(ZK_HOST,{
+		headers:{
+			authorization: token
+		}
+	})
+	  .then(function (response) {
+	    //console.log();
+	   res.send(`User is successfully authenticated<br><b>USER ID:</b><i>${response["data"]["id"]}</i>`)
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+	
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
